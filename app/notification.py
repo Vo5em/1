eb341c2f065538934a5b68f6c0f11a450024742d
Vo5em <volkovkir1611@gmail.com>
2025-08-28 17:15:26 +0300

    u

--- app/notification.py
+++ app/notification.py
@@ -8,6 +8,6 @@ async def notify_before_end(tg_id: int):
 async def notify_end(tg_id: int):
     await bot.send_message(tg_id, "🔴 Ваша подписка истекла.", reply_markup=kb.go_pay)
 
-async def notify_sps(tg_id: int):
+async def notify_sps(tg_id):
     print(f"[DEBUG] sending to {tg_id}")
     await bot.send_message(tg_id, "Тестовое сообщение")
