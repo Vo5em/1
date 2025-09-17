06ca523be32f39123e6a435232b01bd18c2aae68
Vo5em <volkovkir1611@gmail.com>
2025-09-18 00:12:50 +0300

    f

--- app/user.py
+++ app/user.py
@@ -35,7 +35,7 @@ async def cmd_start(message: Message, command: CommandObject):
 
     is_key = await find_key(tg_id)
     if not is_key:
-        await message.photo(
+        await message.edit_media(
             media=file_id03,
             caption=f"<blockquote>ECALONE district 01</blockquote>\n"
                     f"Ты близко.\n\n\n"
@@ -51,14 +51,14 @@ async def cmd_start(message: Message, command: CommandObject):
             is_day = is_day.replace(tzinfo=MOSCOW_TZ)
 
         if is_day < now_moscow:
-            await message.photo(InputMediaPhoto(
+            await message.edit_media(InputMediaPhoto(
                 media=file_id02,
                 caption=f"<blockquote>project echalon;\n district: 01</blockquote>\n\n\n"
                         f"<b>Абонимент не активен.</b>",
                 parse_mode="HTML"),
                 reply_markup=kb.main_old
             )
-        else: await message.photo(InputMediaPhoto(
+        else: await message.edit_media(InputMediaPhoto(
             media=file_id01,
             caption=f"<blockquote>project echalon;\n district: 01</blockquote>\n\n\n"
                     f"<b>Абонимент активен.</b>",
