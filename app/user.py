fe2514541397ec5e4ed9bd4d95982f7cc1d822a7
Vo5em <volkovkir1611@gmail.com>
2025-10-03 15:05:21 +0300

    ger

--- app/user.py
+++ app/user.py
@@ -116,7 +116,7 @@ async def cmd_help(message: Message):
         f"<b>ID:</b><code>{tg_id}</code>\n\n"
         f"Первым сообщением напиши свой ID\n"
         f"Дальше опиши проблему — и мы тебе поможем.",
-        pats_mode="HTML",
+        parse_mode="HTML",
         reply_markup=kb.helps
     )
 
